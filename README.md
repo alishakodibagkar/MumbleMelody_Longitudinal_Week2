c33e4899bec6938a045036f5328ca767d0fc96a8
Alisha Kodibagkar <alishakodibagkar@Alishas-MacBook-Pro.local>
2023-01-18 14:06:39 -0500

    add week 2 materials

---
+++ README.md
@@ -0,0 +1,1 @@
+# This repository contains the Week 1 testing protocol for the Mumble Melody longitudinal study.
---
+++ ui-changes/src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  /* eslint-disable */
+  githubSrc: 'https://raw.githubusercontent.com/alishakodibagkar/MumbleMelody_Longitudinal_Week1/master/protocol/Week1_schema',
+  startButton: {
+    "en": 'Join'
+  },
+  banner: 'Week 1 Testing Session for Mumble Melody Longitudinal Study',
+  assetsPublicPath: '/MumbleMelody_Longitudinal_Week1/',
+  contact: 'akodiba1@mit.edu',
+  showHelp: true,
+  checkMediaPermission: true
+};
+
